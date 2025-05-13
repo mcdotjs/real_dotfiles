6b4df9582fedba4c1f5988873af0e905cfcc11cb
mcdotjs <mirectm@gmail.com>
2025-05-13 16:45:51 +0200

    autoformat with autocmd
    lspAttach

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,24 +2,46 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local augroup = vim.api.nvim_create_augroup
 local MoveNCodeGroup = augroup("moveNcode", {})
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
+
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  -- here is no buffer
+  callback = function(args)
+    -- Get the detaching client
+
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then
+      return
+    end
+
+    if client.supports_method("textDocument/formatting") then
+      -- here we are using current buffer
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+        end,
+      })
+    end
+  end,
+})
 -- vim.api.nvim_create_autocmd("BufEnter", {
 -- 	group = MoveNCodeGroup,
 -- 	callback = function()
@@ -32,11 +54,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- })
 
 vim.diagnostic.config({
-	virtual_text = true, -- Enable virtual text
-	signs = true, -- Enable signs
-	underline = true, -- Enable underline
-	update_in_insert = false, -- Do not update diagnostics in insert mode
-	severity_sort = true, -- Sort diagnostics by severity
+  virtual_text = true,     -- Enable virtual text
+  signs = true,            -- Enable signs
+  underline = true,        -- Enable underline
+  update_in_insert = false, -- Do not update diagnostics in insert mode
+  severity_sort = true,    -- Sort diagnostics by severity
 })
 
 --plugins - are loaded automaticallz from local lua/plugins
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,109 +1,105 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "volar", "ts_ls", "tailwindcss", "cssls", "html", "gopls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			local capabilitiesDefault = require("cmp_nvim_lsp").default_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "volar", "ts_ls", "tailwindcss", "cssls", "html", "gopls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
 
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						workspace = {
-							checkThirdParty = false,
-							library = {
-								"${3rd}/luv/library",
-								unpack(vim.api.nvim_get_runtime_file("", true)),
-							},
-						},
-						completion = {
-							callSnippet = "Replace",
-						},
-					},
-				},
-			})
+    dependencies = {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local capabilitiesDefault = require("cmp_nvim_lsp").default_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					virtual_text = true,
-				},
-				--			cmd = lsp_containers.command("tsserver"),
-				filetypes = { "javascript", "typescript", "vue" },
-				init_options = {
-					plugins = {
-						{
-							name = "@vue/typescript-plugin",
-							location = os.getenv("HOME")
-								.. "/.nvm/versions/node/v20.11.1/lib/node_modules/@vue/typescript-plugin",
-							languages = { "javascript", "typescript", "vue" },
-						},
-					},
-				},
-			})
+      lspconfig.lua_ls.setup({})
 
-			lspconfig.tailwindcss.setup({})
-			lspconfig.prismals.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.html.setup({
-				filetypes = { "vue" },
-				capabilities = capabilitiesDefault,
-			})
-			lspconfig.pylsp.setup({})
-			lspconfig.gopls.setup({})
-			--Keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			--vim.keymap.set("n", "gR", vim.lsp.buf.references, { desc = "[g]o to [R]eferences" })
-			--vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "[g]o to [I]mplementations" })
-			--vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "rn", vim.lsp.buf.rename, { desc = "[r]e[n]ame .. dont know what at the moment" })
-			vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "[g]o to [R]eferences" })
-			vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, { desc = "[g]o to [D]efinitions" })
-			vim.keymap.set(
-				"n",
-				"gI",
-				require("telescope.builtin").lsp_implementations,
-				{ desc = "[g]o to [I]mplementations" }
-			)
-			-- Jump to the type of the word under your cursor.
-			--  Useful when you're not sure what type a variable is and you want to see
-			--  the definition of its *type*, not where it was *defined*.
-			vim.keymap.set(
-				"n",
-				"<leader>D",
-				require("telescope.builtin").lsp_type_definitions,
-				{ desc = "Type [D]efinitions" }
-			)
-			vim.keymap.set(
-				"n",
-				"ds",
-				require("telescope.builtin").lsp_document_symbols,
-				{ desc = "searching [d]ocument [s]ymbols" }
-			)
-			vim.keymap.set(
-				"n",
-				"ws",
-				require("telescope.builtin").lsp_dynamic_workspace_symbols,
-				{ desc = "searching [w]orkspace [s]ymbols" }
-			)
-		end,
-	},
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          virtual_text = true,
+        },
+        --			cmd = lsp_containers.command("tsserver"),
+        filetypes = { "javascript", "typescript", "vue" },
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = os.getenv("HOME")
+                  .. "/.nvm/versions/node/v20.11.1/lib/node_modules/@vue/typescript-plugin",
+              languages = { "javascript", "typescript", "vue" },
+            },
+          },
+        },
+      })
+
+      lspconfig.tailwindcss.setup({})
+      lspconfig.prismals.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.html.setup({
+        filetypes = { "vue" },
+        capabilities = capabilitiesDefault,
+      })
+      lspconfig.pylsp.setup({})
+      lspconfig.gopls.setup({})
+      --Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      --vim.keymap.set("n", "gR", vim.lsp.buf.references, { desc = "[g]o to [R]eferences" })
+      --vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "[g]o to [I]mplementations" })
+      --vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "rn", vim.lsp.buf.rename, { desc = "[r]e[n]ame .. dont know what at the moment" })
+      vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "[g]o to [R]eferences" })
+      vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, { desc = "[g]o to [D]efinitions" })
+      vim.keymap.set(
+        "n",
+        "gI",
+        require("telescope.builtin").lsp_implementations,
+        { desc = "[g]o to [I]mplementations" }
+      )
+      -- Jump to the type of the word under your cursor.
+      --  Useful when you're not sure what type a variable is and you want to see
+      --  the definition of its *type*, not where it was *defined*.
+      vim.keymap.set(
+        "n",
+        "<leader>D",
+        require("telescope.builtin").lsp_type_definitions,
+        { desc = "Type [D]efinitions" }
+      )
+      vim.keymap.set(
+        "n",
+        "ds",
+        require("telescope.builtin").lsp_document_symbols,
+        { desc = "searching [d]ocument [s]ymbols" }
+      )
+      vim.keymap.set(
+        "n",
+        "ws",
+        require("telescope.builtin").lsp_dynamic_workspace_symbols,
+        { desc = "searching [w]orkspace [s]ymbols" }
+      )
+    end,
+  },
 }
