222d88d31b091e5680923637434f288160db171c
mcdotjs <mirectm@gmail.com>
2025-05-14 15:34:50 +0200

    quickfix next prev to alt

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -52,8 +52,8 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 --vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-vim.keymap.set("n", "<C-j>", ":cprev<CR>", { desc = "prev item in quickfix list" })
-vim.keymap.set("n", "<C-k>", ":cnext<CR>", { desc = "next item in quickfix list" })
+vim.keymap.set("n", "<M-j>", ":cprev<CR>", { desc = "prev item in quickfix list" })
+vim.keymap.set("n", "<M-k>", ":cnext<CR>", { desc = "next item in quickfix list" })
 
 --make file executable
 vim.keymap.set("n", "<leader>x", "x :!chmod +x %<CR>", { desc = "make file executable" })
